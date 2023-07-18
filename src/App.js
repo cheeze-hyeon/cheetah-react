1c6f48024673d01a2330135cf81e4bf1d5ece72c
CZ <wngus0223@naver.com>
2023-07-18 12:48:41 +0900

    feat: add route, font-face

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from './routes/HomePage';
+import SignInPage from './routes/SignInPage';
+import SignUpPage from './routes/SignUpPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        {/* home */}
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signin" element={<SignInPage/>} /> 
+        <Route path="/signup" element={<SignUpPage/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
