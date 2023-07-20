19adc4a367c8df4d4a36175fba82a4d74fb607ce
CZ <wngus0223@naver.com>
2023-07-20 22:06:22 +0900

    feat: add data

---
+++ src/data/tags.js
@@ -0,0 +1,39 @@
+const tags = [
+  {
+    id: 1,
+    user_id: 1,
+    title: "죽과이",
+    color: "#FFB35E",
+    is_used: 1,
+  },
+  {
+    id: 2,
+    user_id: 1,
+    title: "심개",
+    color: "#FFD9D9",
+    is_used: 1,
+  },
+  {
+    id: 3,
+    user_id: 1,
+    title: "음속철",
+    color: "#FFFDC1",
+    is_used: 1,
+  },
+  {
+    id: 4,
+    user_id: 1,
+    title: "콘크리트",
+    color: "#FFF48B",
+    is_used: 1,
+  },
+  {
+    id: 5,
+    user_id: 1,
+    title: "물실1",
+    color: "#EBEFF0",
+    is_used: 1,
+  },
+];
+
+export default tags;
---
+++ src/data/users.js
@@ -0,0 +1,11 @@
+const users = [
+  {
+    id: 1,
+    username: "user1",
+    password: "1111",
+    is_autologin: 1,
+  },
+
+];
+
+export default users;
