dd7ad8935157eb7cc357807aa45d3a8bbba38af1
CZ <wngus0223@naver.com>
2023-07-26 04:27:15 +0900

    fix: GoalCard, GoalDetailModal 컴포넌트화

--- src/routes/goal/GoalMainPage.jsx
+++ src/routes/goal/GoalMainPage.jsx
@@ -10,9 +10,9 @@ import { GoalTabBar } from "../../components/tabBar";
 import "tailwindcss/tailwind.css";
 import "../../index.css";
 import GoalDetailModal from "./goaldetailmodal/GoalDetailModal";
-import { calendarMainRoot } from "../calendar/styled";
 import { HeaderTag } from "../../components/header/styled";
 import { TextNormal } from "../../components/text/styled";
+import { calendarMainRoot } from "../calendar/styled";
 
 const GoalMainPage = () => {
   const [selectedTagId, setSelectedTagId] = useState(null);
@@ -83,7 +83,7 @@ const GoalMainPage = () => {
 
         {/* GoalDetailModal을 선택한 goal의 정보로 열어줍니다. */}
         {isModalOpen && (
-          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 flex flex-col w-357 h-500 p-4 gap-4 bg-white shadow-lg rounded-lg">
+          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 flex flex-col bg-white shadow-lg rounded-lg">
             {/* GoalDetailModal 컴포넌트에 todos 더미데이터를 전달합니다. */}
             <GoalDetailModal
               goal={selectedGoal}
--- src/routes/goal/goaldetailmodal/TodoCheck.jsx
+++ src/routes/goal/goaldetailmodal/TodoCheck.jsx
@@ -3,6 +3,7 @@ import CheckFalse from "./CheckFalse";
 import CheckTrue from "./CheckTrue";
 import "tailwindcss/tailwind.css";
 import HeaderClose from "./HeaderClose";
+import { TextLight } from "../../../components/text/styled";
 
 const TodoCheck = ({ todo }) => {
   const [isCompleted, setIsCompleted] = useState(false);
@@ -32,30 +33,55 @@ const TodoCheck = ({ todo }) => {
   }
 
   return (
-    <div className="box-border flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 w-full h-[38px] px-2 bg-white border-t-0 border-r-0 border-b border-l-0 border-neutral-100">
-      <div className="box-border flex justify-between items-center flex-grow basis-full relative">
-        <div className="box-border flex justify-start items-center flex-grow basis-full relative gap-[5px]">
-          {isCompleted ? (
-            <CheckTrue onClick={handleCheckToggle} />
-          ) : (
-            <CheckFalse onClick={handleCheckToggle} />
-          )}
-          <div className="box-border flex justify-start items-center flex-grow-0 flex-shrink-0 w-18 relative gap-2.5">
-            <input
-              type="text"
-              value={title}
-              className={`whitespace-pre-wrap flex-grow font-['Pretendard'] text-[13px] leading-[19px] text-left ${
-                isCompleted ? "text-[#a3a2a4] line-through" : "text-black"
-              }`}
-              onChange={handleTitleChange}
-            />
-          </div>
-        </div>
-        <button onClick={handleDeleteTodo}>
-          <HeaderClose />
-        </button> {/* 추가: 삭제 버튼을 추가합니다. */}
-      </div>
+    <div className="flex flex-row px-[10px] gap-[5px] items-center">
+      {isCompleted ? (
+        <CheckTrue onClick={handleCheckToggle} />
+      ) : (
+        <CheckFalse onClick={handleCheckToggle} />
+      )}
+      <TextLight
+        className={`${
+          isCompleted ? "text-[#a3a2a4]" : "text-black"
+        }`}
+      >
+        <input
+          type="text"
+          value={title}
+          className={`whitespace-pre-wrap w-[240px] mr-[5px]${
+            isCompleted ? "text-[#a3a2a4] line-through" : "text-black"
+          }`}
+          onChange={handleTitleChange}
+        />
+      </TextLight>
+      <button onClick={handleDeleteTodo}>
+        <HeaderClose width="20" height="20" />
+      </button>
     </div>
+    // <div className="box-border flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 w-full h-[38px] px-2 bg-white border-t-0 border-r-0 border-b border-l-0 border-neutral-100">
+    //   <div className="box-border flex justify-between items-center flex-grow basis-full relative">
+    //     <div className="box-border flex justify-start items-center flex-grow basis-full relative gap-[5px]">
+    //       {isCompleted ? (
+    //         <CheckTrue onClick={handleCheckToggle} />
+    //       ) : (
+    //         <CheckFalse onClick={handleCheckToggle} />
+    //       )}
+    //       <div className="box-border flex justify-start items-center flex-grow-0 flex-shrink-0 w-18 relative gap-2.5">
+    //         <input
+    //           type="text"
+    //           value={title}
+    //           className={`whitespace-pre-wrap flex-grow font-['Pretendard'] text-[13px] leading-[19px] text-left ${
+    //             isCompleted ? "text-[#a3a2a4] line-through" : "text-black"
+    //           }`}
+    //           onChange={handleTitleChange}
+    //         />
+    //       </div>
+    //     </div>
+    //     {/* 추가: 삭제 버튼을 추가합니다. */}
+    //     <button onClick={handleDeleteTodo}>
+    //       <HeaderClose width="20" height="20"/>
+    //     </button>
+    //   </div>
+    // </div>
   );
 };
 
