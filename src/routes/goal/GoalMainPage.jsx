bd960579feb4bce32091d1b96cc220ad5b4a3174
jungseo1 <jungseo4723@gmail.com>
2023-07-26 17:22:31 +0900

    골메인페이지 추가수정, 골 추가하는 +버튼 삽입->에러

--- src/routes/goal/GoalMainPage.jsx
+++ src/routes/goal/GoalMainPage.jsx
@@ -13,7 +13,9 @@ import GoalDetailModal from "./goaldetailmodal/GoalDetailModal";
 import { HeaderTag } from "../../components/header/styled";
 import { TextNormal } from "../../components/text/styled";
 import { calendarMainRoot } from "../calendar/styled";
-
+import * as s from "../../../src/routes/calendar/styled";
+import { GoalCreateModal } from "../calendar/goal-create/styled";
+import { ModalOverlay } from "../../components/modal/styled";
 
 const GoalMainPage = () => {
   const [selectedTagId, setSelectedTagId] = useState(null);
@@ -49,53 +51,90 @@ const GoalMainPage = () => {
     setIsModalOpen(false);
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center w-[390px] h-[844px]">
-      <div className="w-full h-full flex flex-col">
-        <HeaderTag text="내 목표" to="/tag-detail" />
-        <div className="h-full bg-[#f5f5f5]">
-          <div className="flex max-w-screen overflow-x-auto scrollbar-hide">
-            <TagList
-              tags={tags}
-              selectedTagId={selectedTagId}
-              onTagClick={handleTagClick}
-            />
-          </div>
-          <div className="flex flex-col m-[20px] gap-[20px] max-w-screen h-full overflow-y-auto">
-            {filteredGoals.length > 0 ? (
-              <div className="flex flex-col gap-[20px] mb-[100px]">
-                <TextNormal>{`${goalCount}개의 목표`}</TextNormal>
-                <div className="flex flex-col gap-[15px]">
-                  {filteredGoals.map((goal) => (
-                    <div key={goal.id}>
-                      {/* GoalCard를 클릭하면 handleGoalCardClick 함수가 호출되도록 합니다. */}
-                      <div onClick={() => handleGoalCardClick(goal.id)}>
-                        <GoalCard goal={goal} />
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ) : (
-              <TextNormal className="text-center">목표가 없습니다.</TextNormal>
-            )}
-          </div>
-        </div>
+  const [isGoalCreateModalOpen, setisGoalCreateModalOpen] = useState(false);
+  const [modalStep, setModalStep] = useState(1);
 
-        {/* GoalDetailModal을 선택한 goal의 정보로 열어줍니다. */}
-        {isModalOpen && (
-          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 flex flex-col bg-white shadow-lg rounded-lg">
-            {/* GoalDetailModal 컴포넌트에 todos 더미데이터를 전달합니다. */}
-            <GoalDetailModal
-              goal={selectedGoal}
-              todos={todos}
-              onCloseModal={handleModalClose}
-            />
+  const showGoalCreateModal = (e) => {
+    if (e.target === e.currentTarget) {
+      setisGoalCreateModalOpen(!isGoalCreateModalOpen);
+      setModalStep(1);
+    }
+  };
+
+  const onClickModalBack = (e) => {
+    if (e.target === e.currentTarget) {
+      setModalStep(1);
+      console.log("hh");
+    }
+  };
+  const addModalStep = () => {
+    setModalStep(2);
+  };
+
+  return (
+    <>
+      <s.calendarMainRoot>
+        <div className="w-full h-full flex flex-col z-0">
+          <HeaderTag text="내 목표" to="/tag-detail" />
+          <div className="h-full bg-[#f5f5f5]">
+            <div className="flex max-w-screen overflow-x-auto scrollbar-hide">
+              <TagList
+                tags={tags}
+                selectedTagId={selectedTagId}
+                onTagClick={handleTagClick}
+              />
+            </div>
+            <div className="flex-col m-[20px] gap-[20px] max-w-screen h-[790px] pb-1000 overflow-y-auto">
+              {filteredGoals.length > 0 ? (
+                <div className="flex flex-col gap-[20px] mb-[100px]">
+                  <TextNormal>{`${goalCount}개의 목표`}</TextNormal>
+                  <div className="flex flex-col gap-[15px] pb-200">
+                    {filteredGoals.map((goal) => (
+                      <div key={goal.id}>
+                        {/* GoalCard를 클릭하면 handleGoalCardClick 함수가 호출되도록 합니다. */}
+                        <div onClick={() => handleGoalCardClick(goal.id)}>
+                          <GoalCard goal={goal}/>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ) : (
+                <TextNormal className="text-center">목표가 없습니다.</TextNormal>
+              )}
+            </div>
           </div>
-        )}
-      </div>
-      <GoalTabBar />
-    </div>
+
+
+        </div>
+        <GoalTabBar />
+        <s.floatingBtnContainer onClick={showGoalCreateModal}/>
+      
+      </s.calendarMainRoot>
+      {isModalOpen && (
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 flex flex-col bg-white shadow-lg rounded-lg">
+              {/* GoalDetailModal 컴포넌트에 todos 더미데이터를 전달합니다. */}
+              <GoalDetailModal
+                goal={selectedGoal}
+                todos={todos}
+                onCloseModal={handleModalClose}
+              />
+            </div>
+          )}
+      {isGoalCreateModalOpen && (
+        <ModalOverlay onClick={showGoalCreateModal}>
+          <GoalCreateModal
+            to1={showGoalCreateModal}
+            to2={addModalStep}
+            step={modalStep}
+            clickBtnClose={showGoalCreateModal}
+            clickBtnBack={onClickModalBack}
+            clickCompleteBtn={showGoalCreateModal}
+            tags={tags}
+          ></GoalCreateModal>
+        </ModalOverlay>
+      )}
+    </>
   );
 };
 
